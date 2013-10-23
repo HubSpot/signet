366871c03c7d22acf6ba2f95491b072b3e3df09a
Adam Schwartz <aschwartz@hubspot.com>
2013-10-23 02:31:22 -0700

    Convert signet to coffee and add coffee grunt config and dep

--- signet.js
+++ signet.js
@@ -1,178 +1,157 @@
-/*
+(function() {
+  var autoInit, deferConsole, draw, getContent, getOptions, orDefault, tag;
 
-    ||||||
-    Signet   Adam Schwartz & Zack Bloom
+  getOptions = function(options) {
+    options.title = orDefault(options.title, getContent('meta[name="application-name"]'), getContent('meta[property="og:title"]'), document.title.split(/\u0020[\/\\\|\-\u8211\u8212]\u0020|\:\u0020/)[0], '');
+    options.author = orDefault(options.author, getContent('meta[name=author]'), '');
+    options.description = orDefault(options.description, getContent('meta[name=description]'), getContent('meta[property="og:description"]'), '');
+    options.image = orDefault(options.image, getContent('meta[property="og:image"]'), getContent('meta[name=image]'));
+    options.hue = options.hue || 0;
+    options.baseStyles = orDefault(options.baseStyles, 'color: #444; font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;');
+    options.titleStyles = orDefault(options.titleStyles, "" + options.baseStyles + "; font-size: 20px; line-height: 30px;");
+    options.authorStyles = orDefault(options.authorStyles, "" + options.baseStyles + "; font-size: 12px; line-height: 30px; padding-left: 20px;");
+    options.descriptionStyles = orDefault(options.descriptionStyles, "" + options.baseStyles + "; font-size: 14px; line-height: 20px;");
+    return options;
+  };
 
-    Display a unique seal in the developer console of your page.
-    https://github.com/HubSpot/signet
-
-    Settings:
-
-    - signet.title             - string  - title of your page (required to show color bars signet)
-    - signet.author            - string  - author of your page
-    - signet.description       - string  - description of your page
-
-    - signet.signet            - boolean - show color bars signet above the title
-    - signet.hue               - integer - hue offset for the color bars
-
-    - signet.baseStyles        - string  - base style string for all parst of the signet (best used to set base font or color)
-    - signet.titleStyles       - string  - title styles
-    - signet.authorStyles      - string  - author styles
-    - signet.descriptionStyles - string  - description styles
-
-*/
-
-(function(){
-    var autoInit, tag;
-
-    window.signet = window.signet || {};
-    window.signet.options = window.signet.options || window.signetOptions || {};
-
-    if (!window.console || !window.console.log || !document.head || !document.querySelector) {
-        window.signet.draw = function() {};
-        return;
+  orDefault = function() {
+    var i;
+    i = 0;
+    while (i <= arguments.length) {
+      if (typeof arguments[i] !== 'undefined') {
+        return arguments[i];
+      }
+      i++;
     }
+    return arguments[arguments.length - 1];
+  };
 
-    autoInit = true;
-    tag = document.querySelector('[data-signet-draw]');
-    if (tag)
-        autoInit = (tag.getAttribute('data-signet-draw').toLowerCase() != 'false');
-    if (signet.options.draw === false)
-        autoInit = false;
+  getContent = function(selector) {
+    var _ref;
+    return (_ref = document.head.querySelector(selector)) != null ? _ref.content : void 0;
+  };
 
-    // Defer execution until the next event loop tick, but don't let anything be rendered to the console
-    // until we can run our function.  This will break console.log line numbers, but only for the first tick.
-    // The fn is passed a special console which will actually log immediately.
-    function deferConsole(fn) {
-        var messages, message, block, old, callable, types, type, i;
-
-        types = ['log', 'debug', 'warn', 'error'];
-        old = {};
-        callable = {};
-        messages = [];
-
-        for (i = types.length; i--;) {
-            (function(type){
-                old[type] = console[type];
-
-                callable[type] = function() {
-                    old[type].apply(console, arguments);
-                };
-
-                console[type] = function() {
-                    messages.push([type, arguments]);
-                };
-            })(types[i]);
-        }
-
-        setTimeout(function(){
-            function then(){
-                while (messages.length) {
-                    block = messages.shift();
-                    type = block[0];
-                    message = block[1];
-
-                    old[type].apply(console, message);
-                }
-
-                for (i = types.length; i--;) {
-                    console[type] = old[type];
-                }
-            };
-
-            fn(callable, then);
-        }, 0);
+  deferConsole = function(fn) {
+    var callable, i, messages, old, type, types, _fn, _i, _len;
+    types = ['log', 'debug', 'warn', 'error'];
+    old = {};
+    callable = {};
+    messages = [];
+    i = types.length;
+    _fn = function(type) {
+      old[type] = console[type];
+      callable[type] = function() {
+        return old[type].apply(console, arguments);
+      };
+      return console[type] = function() {
+        return messages.push([type, arguments]);
+      };
+    };
+    for (i = _i = 0, _len = types.length; _i < _len; i = ++_i) {
+      type = types[i];
+      _fn(type);
     }
-
-    function draw(options, _console, cb) {
-        var i, img, args;
-
-        _console = _console || window.console;
-
-        options = options || window.signet.options || {
-            enabled: true
-        };
-
-        if (options.enabled === false)
-            return;
-
-        function orDefault(){
-            for (i = 0; i <= arguments.length; i++) {
-                if (typeof arguments[i] !== 'undefined')
-                    return arguments[i];
-            }
-            return arguments[arguments.length - 1];
+    return setTimeout((function() {
+      var _then;
+      _then = function() {
+        var block, message, _results;
+        while (messages.length) {
+          block = messages.shift();
+          type = block[0];
+          message = block[1];
+          old[type].apply(console, message);
         }
-
-        function getContent(selector) {
-            var el = document.head.querySelector(selector);
-
-            if (el)
-                return el.content;
-            return undefined;
+        i = types.length;
+        _results = [];
+        while (i--) {
+          _results.push(console[type] = old[type]);
         }
+        return _results;
+      };
+      return fn(callable, _then);
+    }), 0);
+  };
 
-        options.title = orDefault(options.title, getContent('meta[name="application-name"]'), getContent('meta[property="og:title"]'), document.title.split(/ [\/\\\|\-\u8211\u8212] |\: /)[0], '');
-        options.author = orDefault(options.author, getContent('meta[name=author]'), '');
-        options.description = orDefault(options.description, getContent('meta[name=description]'), getContent('meta[property="og:description"]'), '');
-
-        options.image = orDefault(options.image, getContent('meta[property="og:image"]'), getContent('meta[name=image]'));
-
-        options.hue = options.hue || 0;
-
-        options.baseStyles = orDefault(options.baseStyles, 'color: #444; font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;');
-
-        options.titleStyles = orDefault(options.titleStyles, options.baseStyles + ';font-size: 20px; line-height: 30px;');
-        options.authorStyles = orDefault(options.authorStyles, options.baseStyles + ';font-size: 12px; line-height: 30px; padding-left: 20px;');
-        options.descriptionStyles = orDefault(options.descriptionStyles, options.baseStyles + ';font-size: 14px; line-height: 20px;');
-
-        function _draw() {
-            if (options.title) {
-                if (!options.image) {
-                    args = [''];
-                    for (i = 0; i < options.title.length; i++) {
-                        args[0] += '%c' + options.title[i];
-                        if (options.title[i] === ' ') {
-                            args.push(options.titleStyles);
-                        } else {
-                            args.push(options.titleStyles + '; background: hsl(' + (((options.title[i].toLowerCase().charCodeAt(0) * 2) + options.hue) % 255) + ', 80%, 80%); color: transparent; line-height: 0;');
-                        }
-                    }
-                    _console.log.apply(console, args);
-                }
-
-                if (options.author)
-                    _console.log('%c' + options.title + '%c' + options.author, options.titleStyles, options.authorStyles);
-                else
-                    _console.log('%c' + options.title, options.titleStyles);
-            }
-
-            if (options.description)
-                _console.log('%c' + options.description, options.descriptionStyles);
-
-            if (cb)
-                cb();
-        }
-
+  draw = function(options, _console, cb) {
+    var img, _draw;
+    _draw = function() {
+      var args, hue, i;
+      if (options.title) {
         if (!options.image) {
-            _draw();
-        } else {
-            img = new Image();
-
-            img.onload = function() {
-                _console.log('%c ', 'font-size: 0; line-height: ' + img.height + 'px; padding: ' + Math.floor(img.height / 2) + 'px ' + img.width + 'px ' + Math.ceil(img.height / 2) + 'px 0; background-image: url("' + img.src + '");');
-                _draw();
-            };
-
-            img.src = options.image;
+          args = [''];
+          i = 0;
+          while (i < options.title.length) {
+            args[0] += "%c" + options.title[i];
+            if (options.title[i] === ' ') {
+              args.push(options.titleStyles);
+            } else {
+              hue = ((options.title[i].toLowerCase().charCodeAt(0) * 2) + options.hue) % 255;
+              args.push("" + options.titleStyles + "; background: hsl(" + hue + ", 80%, 80%); color: transparent; line-height: 0;");
+            }
+            i++;
+          }
+          _console.log.apply(console, args);
         }
+        if (options.author) {
+          _console.log("%c" + options.title + "%c" + options.author, options.titleStyles, options.authorStyles);
+        } else {
+          _console.log("%c" + options.title, options.titleStyles);
+        }
+      }
+      if (options.description) {
+        _console.log("%c" + options.description, options.descriptionStyles);
+      }
+      if (cb) {
+        return cb();
+      }
+    };
+    _console = _console || window.console;
+    options = options || window.signet.options || {
+      enabled: true
+    };
+    if (options.enabled === false) {
+      return;
     }
-
-    if (autoInit) {
-        deferConsole(function(_console, then){
-            draw(null, _console, then);
-        });
+    options = getOptions(options);
+    if (!options.image) {
+      return _draw();
+    } else {
+      img = new Image();
+      img.onload = function() {
+        _console.log('%c ', "font-size: 0; line-height: " + img.height + "px; padding: " + (Math.floor(img.height / 2)) + "px " + img.width + "px " + (Math.ceil(img.height / 2)) + "px 0; background-image: url(\"" + img.src + "\");");
+        return _draw();
+      };
+      return img.src = options.image;
     }
+  };
 
-    window.signet.draw = draw;
-})();
+  window.signet = window.signet || {};
+
+  window.signet.options = window.signet.options || window.signetOptions || {};
+
+  if (!window.console || !window.console.log || !document.head || !document.querySelector) {
+    window.signet.draw = function() {};
+    return;
+  }
+
+  autoInit = true;
+
+  tag = document.querySelector('[data-signet-draw]');
+
+  if (tag) {
+    autoInit = tag.getAttribute('data-signet-draw').toLowerCase() !== 'false';
+  }
+
+  if (signet.options.draw === false) {
+    autoInit = false;
+  }
+
+  if (autoInit) {
+    deferConsole(function(_console, _then) {
+      return draw(null, _console, _then);
+    });
+  }
+
+  window.signet.draw = draw;
+
+}).call(this);
